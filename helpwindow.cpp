a9871422314fc8e3cb5c0e0a117eca7f45f9f70e
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-09-27 17:56:21 +0000

    Собрал все в кучу

--- helpwindow.cpp
+++ helpwindow.cpp
@@ -1,17 +1,25 @@
 #include "helpwindow.h"
 #include "ui_helpwindow.h"
-
-#ifdef WIN32
-
-#else
-
-#endif
-
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 HelpWindow::HelpWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    QString app_path=QApplication::applicationDirPath();
+    QString delimiter="/";
+    if (app_path.contains('\\'))
+        delimiter="\\";
+    QFile fl(app_path+delimiter+"help.htm");
+    if (fl.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+     QTextStream s(&fl);
+     ui->helpView->setHtml(s.readAll());
+     fl.close();
+    }
+    else QMessageBox::warning(NULL,"Error!",app_path+delimiter+"help.htm: can't be opened!");
 }
 
 HelpWindow::~HelpWindow()
