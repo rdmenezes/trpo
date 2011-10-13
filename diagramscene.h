e26bf2fc3157a67d709e8afab8027a6b418a9e44
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-10-13 18:37:23 +0000

    Добавил сниппет для появления панели. Саму панель сделать не успел.

--- diagramscene.h
+++ diagramscene.h
@@ -2,15 +2,20 @@
 #define DIAGRAMSCENE_H
 
 #include <QGraphicsScene>
+#include <QGraphicsView>
 
 class DiagramScene : public QGraphicsScene
 {
 Q_OBJECT
+private:
+    QGraphicsView *   m_view; //!< View for data
 public:
     explicit DiagramScene(QObject *parent = 0);
 
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
     void keyPressEvent(QKeyEvent * event);
+
+    inline void setView(QGraphicsView * v) { m_view=v; }
 signals:
 
 public slots:
