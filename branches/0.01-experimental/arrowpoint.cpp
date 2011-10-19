21b4ee09f6c5b868baf846419c91878875950fd7
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-10-19 18:42:31 +0000

    Добавил тестирующие функции для точки.

--- branches/0.01-experimental/arrowpoint.cpp
+++ branches/0.01-experimental/arrowpoint.cpp
@@ -6,6 +6,11 @@ ArrowPoint::ArrowPoint(qreal x, qreal y): QPointF(x,y)
 }
 
 
+bool ArrowPoint::isBeginPoint() { return m_out.size()==0; }
+bool ArrowPoint::isEndingPoint() { return m_in.size()==0; }
+
+bool ArrowPoint::mustBeDecomposed() { return isEndingPoint() && m_out.size()>1; }
+bool ArrowPoint::isSeparated()      { return isBeginPoint() && isEndingPoint(); }
 
 ArrowPoint::~ArrowPoint()
 {
