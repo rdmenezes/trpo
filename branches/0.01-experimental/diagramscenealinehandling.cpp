7a2cea7da6a90d008811d70a9e969fbb396438ec
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-10-22 15:49:41 +0000

    Сделал  изменение размера линии аннотации.

--- branches/0.01-experimental/diagramscenealinehandling.cpp
+++ branches/0.01-experimental/diagramscenealinehandling.cpp
@@ -98,12 +98,29 @@ void DiagramScene::processAnnotationLineToSegment(const QPointF & pos,
 void DiagramScene::enterAnnotationLineResize(const QPointF & pos,
                                              ALineItem * item)
 {
-
+ (void)pos;
+  m_dragstate=DS_ALINE_RESIZE;
+  m_resizing_aline=item;
 }
 
 void DiagramScene::leaveAnnotationLineResize(const QPointF & pos)
 {
+  QPointF beg=*(m_resizing_aline->begin());
+  ArrowDirection dir=direction(*(m_resizing_aline->begin()),
+                               *(m_resizing_aline->end()));
+  QPointF newpos=pos;
+  if (dir==AD_TOP || dir==AD_BOTTOM) newpos.setX(m_resizing_aline->begin()->x());
+  else                               newpos.setY(m_resizing_aline->begin()->y());
 
+  bool nonequaldirection=  dir!=direction(*(m_resizing_aline->begin()),newpos);
+  bool toosmall=(fabs(beg.x()-newpos.x())+
+                 fabs(beg.y()-newpos.y()))<MINIMAL_LINE_LENGTH;
+  if (!toosmall || !nonequaldirection)
+  {
+      m_resizing_aline->end()->setX(newpos.x());
+      m_resizing_aline->end()->setY(newpos.y());
+      this->update();
+  }
 }
 
 void DiagramScene::removeAnnotationLine(ALineItem * line)
