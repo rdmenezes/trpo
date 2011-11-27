1c780f924b7f64968fbb4ce2ac79352a77d5b151
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-11-27 17:23:55 +0000

    Переделал архитектуру. Теперь должно быть несколько лучше и с меньшим  числом классов.

--- collisionobject.h
+++ collisionobject.h
@@ -17,61 +17,40 @@ enum  ShapeType
   ST_COMPLEX
 };
 
+/*! Exception, which is thrown if attempted to work with
+    object of one collision type, like another
+ */
+class CollisionDataNotSpecified {
+public:
+      inline CollisionDataNotSpecified(){}
+};
 /*! Marker interface, that determines, whether object is collidable or not
  */
 class CollisionObject
 {
+private:
+        ShapeType  m_shapetype;
 public:
+       inline CollisionObject(ShapeType type) { m_shapetype=type; }
        /*! Determines collision shape of item
            \return collision shape of item
         */
-       virtual ShapeType collisionShape() const =0;
+       inline ShapeType collisionShape() const { return m_shapetype; }
+       /*! Returns a collision rect for item
+        */
+       virtual const QRectF & collisionRect() const;
+       /*! Returns a collision rect for item
+        */
+       virtual const QLineF & collisionLine() const ;
+       /*! Returns first object for collision
+       */
+       virtual CollisionObject *    firstObject();
+       /*! Returns next object for collision
+        */
+       virtual CollisionObject *    nextObject();
        /*! Virtual destructor
         */
        virtual ~CollisionObject();
-};
-
-/*! A rectangle-based collision object
- */
-class  RectangularCollisionObject: public CollisionObject
-{
- public:
-    /*! Determines collision shape of item
-        \return collision shape of itemû
-     */
-    ShapeType collisionShape() const;
-    /*! Returns a collision rect for item
-     */
-    virtual const QRectF & collisionRect() const =0;
-};
-/*! A line-based collision object
- */
-class  LinearCollisionObject: public CollisionObject
-{
-public:
-    /*! Determines collision shape of item
-        \return collision shape of itemû
-     */
-    ShapeType collisionShape() const;
-    /*! Returns a collision rect for item
-     */
-    virtual const QLineF & collisionLine() const =0;
-};
-/*! A complex collision object, that consists from other objects
-*/
-class  ComplexCollisionObject: public CollisionObject
-{
- public:
-        /*! Determines collision shape of item
-            \return collision shape of itemû
-        */
-       ShapeType collisionShape() const;
-       /*! Returns first object for collision
-       */
-       virtual CollisionObject *    firstObject()=0;
-       /*! Returns next object for collision
-        */
-       virtual CollisionObject *    nextObject()=0;
 };
 
 #endif // COLLISIONOBJECT_H
---
+++ serializable.h
@@ -0,0 +1,72 @@
+/*!  \file     serializable.h
+     \author   MamontovDmitry
+     Describes a serializable object, which can be restored from tags
+*/
+#ifndef SERIALIZABLE_H
+#define SERIALIZABLE_H
+#include <QDomDocument>
+#include <QDomElement>
+#include <QMap>
+#include <QString>
+/*! \class Serializable
+    Serializable interface, which describes an item data
+ */
+class Serializable
+{
+public:
+      /*!  Saves a data to document
+           \param[in] doc     document data
+           \param[in] element parent element data
+       */
+      virtual  void save(QDomDocument * doc,QDomElement * element)=0;
+      /*! Loads a default data from document populating address map
+          \param[in] element element data
+          \param[in] addressMap adressedMap
+       */
+      virtual  void load(QDomElement * element,
+                         QMap<void *, Serializable *> & addressMap)=0;
+      /*! Resolves inner pointers, from stored in adress map
+          \param[in] addressMap map of addresses
+       */
+      virtual  void resolvePointers(QMap<void *, Serializable *> & adressMap)=0;
+      /*! Serializable destructor data
+       */
+      virtual ~Serializable();
+};
+
+
+/*! Function, that creates an object for tag.
+ */
+template<typename T>
+Serializable * create() { return new T(); }
+/*! Class, that creates object, corresponding tags
+    He needs initialization on start, and any object, must have
+    default constructor
+ */
+class SerializableFactory
+{
+
+  private:
+         /*! Map String -> creator, used to create data.
+          */
+         static QMap<QString, Serializable * (*)()> m_tagmap;
+  public:
+         /*! Binds a new creator for serializable
+          */
+         template<typename T>
+         static void bind(const QString & name)
+         {
+             m_tagmap.insert(name,create<T>);
+         }
+         /*! Creates an object, corresponding to tag
+          */
+         static inline Serializable * create(const QString & tag)
+         {
+            if (m_tagmap.contains(tag))
+                return m_tagmap[tag]();
+            else
+                return NULL;
+         }
+};
+
+#endif // SERIALIZABLE_H
