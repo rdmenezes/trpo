c46e84aee171928d6774decd36c98f5eda296e59
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-11-30 16:44:56 +0000

    Забыл добавить тесты.

---
+++ graphicunittests.h
@@ -0,0 +1,20 @@
+/*! \file graphicunittests.h
+    \author Mamontov
+    This class performs a unit tests, used for graphics
+ */
+#ifndef GRAPHICUNITTESTS_H
+#define GRAPHICUNITTESTS_H
+
+class Diagram;
+
+class GraphicUnitTests
+{
+private:
+       Diagram * m_diag;
+public:
+        inline GraphicUnitTests(Diagram * d)  {m_diag=d;}
+        void performBoxVerticalTextRegenTest();
+        void performFreeCommentTest();
+};
+
+#endif // GRAPHICUNITTESTS_H
