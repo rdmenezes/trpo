356847578bd5e95b1cbc77362f03d22177a54234
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-11-30 16:44:56 +0000

    Забыл добавить тесты.

---
+++ trunk/graphicunittests.cpp
@@ -0,0 +1,44 @@
+#include "graphicunittests.h"
+#include "diagramscene.h"
+#include "diagram.h"
+#include "box.h"
+#include "freecomment.h"
+#include <iostream>
+
+void GraphicUnitTests::performBoxVerticalTextRegenTest()
+{
+    Box * t=new Box(QPointF(100,25),m_diag);
+    QRectF tmp=t->boundingRect();
+    m_diag->scene()->addItem(t);
+
+    for (int i=1;i<5;i++)
+    {
+      Box * box=new Box(QPointF (100,50
+                                     +(tmp.height()+8)*i),
+                        m_diag);
+      m_diag->scene()->addItem(box);
+      //QRectF r=box->boundingRect();
+      //std::cout<<r.x();
+      //r.setX(box->x());
+      //r.setY(box->y());
+      //r.setWidth(r.width()+2*i);
+      //box->setRect(r);
+      //box->setText("Text\ngrazedaaaaa!");
+    }
+
+}
+
+void GraphicUnitTests::performFreeCommentTest()
+{
+  FreeComment * fc=new FreeComment(QPointF(50,50),m_diag);
+  m_diag->scene()->addItem(fc);
+  /*
+  FreeComment * fc2=new FreeComment(QPointF(100,100),m_diag);
+  m_diag->scene()->addItem(fc2);
+  fc2->setText("Complex comment\nnow simple");
+  FreeComment * fc3=new FreeComment(QPointF(200,100),m_diag);
+  m_diag->scene()->addItem(fc3);
+  fc3->setText("Complex comment\nnow simple");
+  fc3->moveTo(QPointF(200,200));
+  */
+}
