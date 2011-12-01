aa019bb0bee2d5731b8e8e78f29c4c1030fe24cd
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-12-01 19:40:13 +0000

    Исправил неоднозначность в вызове pow().

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -183,35 +183,35 @@ void MainWindow::scale(int coef){
      switch (coef)
      {
      case 0:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=0;
          break;
      case 1:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=1;
          break;
      case 2:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=2;
          break;
      case 3:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=3;
          break;
      case 4:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=4;
          break;
      case 5:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=5;
          break;
      case 6:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=6;
          break;
      case 7:
-         view->scale(pow(2, coef-previousScaleValue), pow(2, coef-previousScaleValue));
+         view->scale(pow(2.0, coef-previousScaleValue), pow(2.0, coef-previousScaleValue));
          previousScaleValue=7;
          break;
      }
