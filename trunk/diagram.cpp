90193b28cf5d9a48ec0fdc48de0dbcbbf32e6cfd
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-12-03 08:37:45 +0000

    Реализовал частично добавление объекта, получение идентификатора и прочее.

--- trunk/diagram.cpp
+++ trunk/diagram.cpp
@@ -6,6 +6,7 @@
 #include "arrowpoint.h"
 #include "arrowsegment.h"
 #include "diagramset.h"
+#include "collisiondetector.h"
 
 int Diagram::id() const
 {
@@ -14,8 +15,20 @@ int Diagram::id() const
 
 int Diagram::getBoxNumber(DiagramObject * b)
 {
-    //!< TODO: Implement this later.
-    return 0;
+    if (m_boxes.contains(static_cast<Box*>(b)))
+    {
+        return m_boxes[static_cast<Box*>(b)];
+    }
+    int i=0;
+    bool hasID=false;
+    do
+    {
+        hasID=m_boxes.values().contains(i);
+        if (hasID)
+            ++i;
+    }
+    while (hasID);
+    return i;
 }
 
 void Diagram::save(QDomDocument * /* doc */,
@@ -37,61 +50,77 @@ void Diagram::resolvePointers(QMap<void *, Serializable *> &
 }
 
 
+bool Diagram::canPlace(DiagramObject * obj,
+                        const QVector<int> & exctypes,
+                        const QVector<DiagramObject *> excobjs
+                      )
+{
+    bool can=true;
+    CollisionDetector * detector=new CollisionDetector();
+    for (int i=0;i<m_objects.size();i++)
+    {
+        if (!exctypes.contains(m_objects[i]->type()) && !excobjs.contains(m_objects[i]))
+        {
+            can= can && !( detector->test(obj,m_objects[i]) );
+        }
+    }
+    delete detector;
+    return can;
+}
+
+
 
 bool Diagram::isCorrect()
 {
+    /*
     for (int i=0;i<DIAGRAM_MAX_BLOCKS;i++)
         if (m_boxes[i]) return true;
     return false;
+    */
 }
 
 Diagram::Diagram()
 {
     m_parent=DiagramParent(-1,-1);
-    nullifyBoxes();
 }
 
 Diagram::Diagram(const DiagramParent&  l)
 {
     m_parent=l;
-    nullifyBoxes();
 }
 
-void Diagram::nullifyBoxes()
-{
-  for(int i=0;i<DIAGRAM_MAX_BLOCKS;i++)
-  {
-    m_boxes[i]=NULL;
-  }
-}
 
 bool Diagram::empty() const
 {
-  //!< TODO: Implement this later
-  return false;
+  return m_boxes.size()==0;
 }
+
+void Diagram::add(DiagramObject * obj)
+{
+  if (m_objects.contains(obj))
+      return;
+  if (obj->type()==IsBox)
+  {
+      int id=getBoxNumber(obj);
+      m_boxes.insert(static_cast<Box*>(obj),id);
+  }
+  m_objects<<obj;
+}
+
 
 bool Diagram::canAddBoxes() const
 {
- for (int i=0;i<DIAGRAM_MAX_BLOCKS;i++)
- {
-     if (!m_boxes[i]) return true;
- }
- return false;
+ return true;
 }
 
 int Diagram::getBoxID() const
 {
- for (int i=0;i<DIAGRAM_MAX_BLOCKS;i++)
- {
-     if (!m_boxes[i]) return i;
- }
- return false;
+  return 0;
 }
 
 void Diagram::addBox(Box * box)
 {
-  this->m_boxes[box->id()]=box;
+  //this->m_boxes[box->id()]=box;
 }
 
 void Diagram::addAnnotationLabel(FreeComment * label)
@@ -114,6 +143,7 @@ void Diagram::removeAnnotationLabel(FreeComment * label)
 bool Diagram::testForNoBlockCollision(const QRectF & rect, QGraphicsItem * pointer)
 {
     //Scan blocks
+    /*
     for (int i=0;i<DIAGRAM_MAX_BLOCKS;i++)
     {
       if (m_boxes[i]!=NULL && m_boxes[i]!=pointer)
@@ -122,6 +152,7 @@ bool Diagram::testForNoBlockCollision(const QRectF & rect, QGraphicsItem * point
               return false;
       }
     }
+    */
     return true;
 }
 
@@ -182,13 +213,15 @@ bool Diagram::canBePlaced(const QRectF & rect, Box * pointer)
 
 Box * Diagram::getBlockByID(int id)
 {
-   return m_boxes[id];
+   //return m_boxes[id];
+   return 0;
 }
 
 
 
 void Diagram::setBlockID(Box * item, char pos)
 {
+    /*
     if (item)
     {
      for(int i=0;i<DIAGRAM_MAX_BLOCKS;i++)
@@ -198,6 +231,7 @@ void Diagram::setBlockID(Box * item, char pos)
     m_boxes[pos]=item;
     if (item)
         item->setID(pos);
+    */
 }
 
 void Diagram::addAnnotationLine(CommentLine * line)
@@ -303,6 +337,7 @@ bool Diagram::canBePlacedAroundPoints(const QRectF & rect, const QVector<ArrowPo
 bool Diagram::canPlaceSegment(ArrowPoint * point1, ArrowPoint * point2,
                               Box * block, bool always_check_bounds)
 {
+    /*
     for (int i=0;i<DIAGRAM_MAX_BLOCKS;i++)
     {
      if (m_boxes[i]!=NULL)
@@ -321,6 +356,7 @@ bool Diagram::canPlaceSegment(ArrowPoint * point1, ArrowPoint * point2,
         //if (collides(m_alabels[i]->boundingRect(),*point1,*point2))
         //    return false;
     }
+    */
     return true;
 }
 
@@ -353,6 +389,6 @@ void Diagram::clear()
  m_alines.clear();
  m_arrow_points.clear();
  m_arrow_segments.clear();
- nullifyBoxes();
+ //nullifyBoxes();
  //m_history.clear();
 }
