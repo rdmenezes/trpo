1da1c9428bc271f17dddc039eae1e9d03266239b
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-12-08 19:18:12 +0000

    Исправил проблемы компиляции

--- trunk/selecttoolstates.cpp
+++ trunk/selecttoolstates.cpp
@@ -2,6 +2,15 @@
 
 void ObjectChangingData::extractData(ObjectConnector * c)
 {
+ QVector< QPair<qreal,ObjectConnector*> > input=c->getConnected(C_INPUT);
 
+
+}
+
+
+void ObjectChangingData::extractData(ObjectConnector * c,
+                                     Connection ct,
+                                     const QVector< QPair<qreal,ObjectConnector *> > & from)
+{
 
 }
