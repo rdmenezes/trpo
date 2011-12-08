b1a82533ceb0aad0168c4dc1d8b0b639d1e811f6
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-12-08 19:33:31 +0000

    Сделал выборку достраиваемых сегментов из класса.

--- trunk/selecttoolstates.cpp
+++ trunk/selecttoolstates.cpp
@@ -3,8 +3,9 @@
 void ObjectChangingData::extractData(ObjectConnector * c)
 {
  QVector< QPair<qreal,ObjectConnector*> > input=c->getConnected(C_INPUT);
-
-
+ extractData(c,C_INPUT,input);
+ QVector< QPair<qreal,ObjectConnector*> > output=c->getConnected(C_OUTPUT);
+ extractData(c,C_OUTPUT,input);
 }
 
 
@@ -12,5 +13,27 @@ void ObjectChangingData::extractData(ObjectConnector * c,
                                      Connection ct,
                                      const QVector< QPair<qreal,ObjectConnector *> > & from)
 {
-
+  for (int i=0;i<from.size();i++)
+  {
+    QPair<qreal,ObjectConnector *> temp=from[i];
+    qreal pos=temp.first;
+    ObjectConnector * con=temp.second;
+    if (con->parent() && con->parent()->type()==IsCommentLine)
+    {
+        CommentLine * cl=static_cast<CommentLine*>(con->parent());
+        m_commentlines<<cl;
+        m_commentlinesposition<<QLineF(cl->in().x(),cl->in().y(),cl->out().x(),cl->out().y());
+    }
+    else
+    {
+        m_attachedconnectors<<c;
+        m_connected_objects<<con;
+        m_connection_direction<<ct;
+        m_poses<<pos;
+        if (ct==C_INPUT)
+            m_reachedpos<<c->p2();
+        else
+            m_reachedpos<<c->p1();
+    }
+  }
 }
