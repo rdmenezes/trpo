1717e40b8953914e9d4151e5a8fe53ea707bf2f8
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2011-12-10 17:09:25 +0000

    Частично реализована отмена/повтор.

--- attachedcomment.cpp
+++ attachedcomment.cpp
@@ -115,9 +115,15 @@ DiagramObject * AttachedComment::clone()
 bool AttachedComment::deleteOnRemoval()
 {
   if (m_comment)
+  {
       m_comment->scene()->removeItem(m_comment);
+      delete m_comment;
+  }
   if (m_line)
+  {
       m_line->scene()->removeItem(m_line);
+      delete m_line;
+  }
   return false;
 }
 
