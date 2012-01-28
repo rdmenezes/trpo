399bbd528ea1c473ea768a37598d532a2fa6a025
overanalyzer1@gmail.com <overanalyzer1@gmail.com@33398881-1f61-4214-7758-fb6bde8b9c1a>
2012-01-28 19:53:39 +0000

    Исправил баг релиза. Теперь при отмене добавления линии комментария не вылетает. (По крайней мере на тех 20 попытках, что были сделаны)

--- trunk/attachedcomment.cpp
+++ trunk/attachedcomment.cpp
@@ -114,15 +114,15 @@ DiagramObject * AttachedComment::clone()
 
 bool AttachedComment::deleteOnRemoval()
 {
-  if (m_comment)
-  {
-      m_comment->scene()->removeItem(m_comment);
-      delete m_comment;
-  }
   if (m_line)
   {
       m_line->scene()->removeItem(m_line);
       delete m_line;
+  }
+  if (m_comment)
+  {
+      m_comment->scene()->removeItem(m_comment);
+      delete m_comment;
   }
   return false;
 }
